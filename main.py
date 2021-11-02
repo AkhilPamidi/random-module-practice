acf50210a922d1b97abf8cf31882fa5398f58552
AkhilChowdary2 <91675151+coderockzz@users.noreply.github.com>
2021-11-02 04:51:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+a=(random.randint(1,2))
+if a==1:
+  print("heads")
+else:
+  print("tails")
+
+
+
+
+
+
+
+
+
+
